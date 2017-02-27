06b15e3cdc81f398c1756b8fe9d474e6bf1746dc
LCH <765353128@qq.com>
2017-02-27 13:49:10 +0800

    对比超级微信，添加EaseUI中的实体类及相关代码

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -10,6 +10,7 @@ import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.controller.EaseUI.EaseUserProfileProvider;
 import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.domain.User;
 
 public class EaseUserUtils {
     
@@ -27,7 +28,20 @@ public class EaseUserUtils {
     public static EaseUser getUserInfo(String username){
         if(userProvider != null)
             return userProvider.getUser(username);
-        
+
+        return null;
+    }
+
+    /**
+     * get User according username
+     *
+     * @param username
+     * @return
+     */
+    public static User getAppUserInfo(String username) {
+        if (userProvider != null)
+            return userProvider.getAppUser(username);
+
         return null;
     }
     
@@ -63,5 +77,73 @@ public class EaseUserUtils {
         	}
         }
     }
-    
+
+    /**
+     * set Appuser's nickname
+     */
+    public static void setAppUserNick(String username, TextView textView) {
+        if (textView != null) {
+            User user = getAppUserInfo(username);
+            if (user != null && user.getMUserNick() != null) {
+                textView.setText(user.getMUserNick());
+            } else {
+                textView.setText(username);
+            }
+        }
+    }
+
+    /**
+     * set user avatar
+     *
+     * @param username
+     */
+    public static void setAppUserAvatar(Context context, String username, ImageView imageView) {
+        User user = getAppUserInfo(username);
+        if (user != null && user.getAvatar() != null) {
+            setAppUserAvatarByPath(context, user.getAvatar(), imageView, null);
+        } else if (username != null) {
+            user = new User(username);
+            setAppUserAvatarByPath(context, user.getAvatar(), imageView, null);
+        } else {
+            Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
+        }
+    }
+
+    public static void setAppUserAvatarByPath(Context context, String path, ImageView iamgeview, String groupId) {
+        int default_avatar = R.drawable.default_hd_avatar;
+        if (groupId != null) {
+            default_avatar = R.drawable.ease_group_icon;
+        }
+        if (path != null) {
+            try {
+                int avatarResId = Integer.parseInt(path);
+                Glide.with(context).load(avatarResId).into(iamgeview);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(path).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(default_avatar).into(iamgeview);
+            }
+        } else {
+            Glide.with(context).load(default_avatar).into(iamgeview);
+        }
+    }
+
+    public static String getGroupAvatarPath(String hxid) {
+        String path = "http://101.251.196.90:8000/SuperWeChatServerV2.0/downloadAvatar?name_or_hxid="
+                + hxid + "&avatarType=group_icon&m_avatar_suffix=.jpg";
+        return path;
+    }
+
+    public static void setAppGroupAvatar(Context context, String hxid, ImageView iamgeview) {
+        if (hxid != null) {
+            try {
+                int avatarResId = Integer.parseInt(getGroupAvatarPath(hxid));
+                Glide.with(context).load(avatarResId).into(iamgeview);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(getGroupAvatarPath(hxid)).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_group_icon).into(iamgeview);
+            }
+        } else {
+            Glide.with(context).load(R.drawable.ease_group_icon).into(iamgeview);
+        }
+    }
 }
