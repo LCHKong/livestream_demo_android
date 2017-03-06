719824a00de4e14c9b45c30e0b8919fa5101d729
LCH <765353128@qq.com>
2017-03-06 11:06:02 +0800

    我的零钱布局的导入

--- app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
@@ -1,16 +1,29 @@
 package cn.ucai.live.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.BaseAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.live.R;
 
 public class ChangeActivity extends BaseActivity {
+
+    @BindView(R.id.tv_change_balance)
+    TextView mTvChangeBalance;
+    @BindView(R.id.target_layout)
+    LinearLayout mTargetLayout;
+    View loadingview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change);
+        ButterKnife.bind(this);
+        loadingview = LayoutInflater.from(ChangeActivity.this).inflate(R.layout.rp_loading, mTargetLayout, false);
+        mTargetLayout.addView(loadingview);
     }
 }
